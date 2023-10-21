e5fd3e65ecd987bff58195d87dd95c7baf70ec5b
Vivek <vivekraj78965.vr@gmail.com>
2023-10-21 16:40:36 +0530

    API to search

--- routes/blog.js
+++ routes/blog.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const blogController = require('../controllers/blogController')
 
 router.get("/blog-stats", blogController.fetchData);
+router.get("/blog-search", blogController.searchData)
 
 module.exports = router;
